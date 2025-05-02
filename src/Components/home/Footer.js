177afe5e05dd43c3823eaf4acf59642b7157e56e
Ashish Raghav <158624214+Ashiiis@users.noreply.github.com>
2025-05-02 20:38:40 +0530

    Update Footer.js

--- src/Components/home/Footer.js
+++ src/Components/home/Footer.js
@@ -34,7 +34,7 @@ export default function Footer() {
       </div>
       <hr className="my-6 border-dark-200 sm:mx-auto dark:border-dark-700 lg:my-8" />
       <div className="sm:flex sm:items-center sm:justify-between">
-          <span className="text-sm text-dark-500 sm:text-center dark:text-dark-400">© 2023 <a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
+          <span className="text-sm text-dark-500 sm:text-center dark:text-dark-400">© <p>All Right are reserved by Ashish Raghav and Kunal Singh (CRSSIET)</p><a href="https://flowbite.com/" className="hover:underline">Flowbite™</a>. All Rights Reserved.
           </span>
           <div className="flex mt-4 sm:justify-center sm:mt-0">
               <a href="/" className="text-dark-500 hover:text-dark-900 dark:hover:text-dark">
